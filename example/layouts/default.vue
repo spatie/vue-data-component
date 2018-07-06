cb4bcb00fe8e948cd4710fb249b57da20fe1731a
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-07-06 17:56:51 +0200

    Docs

--- example/layouts/default.vue
+++ example/layouts/default.vue
@@ -1,6 +1,23 @@
 <template>
-    <div>
-        <nuxt/>
+    <div class="max-w-xl mx-auto py-4 flex min-h-screen">
+        <nav class="w-1/5 pt-8 text-xl">
+            <ul class="mb-8">
+                <li><nuxt-link to="/">Home</nuxt-link></li>
+            </ul>
+            <h2 class="mb-1 text-grey-dark">
+                Examples
+            </h2>
+            <ul>
+                <li><nuxt-link to="/ajax-cards">Ajax cards layout</nuxt-link></li>
+                <li><nuxt-link to="/ajax-table">Ajax table layout</nuxt-link></li>
+            </ul>
+        </nav>
+        <div class="bg-white p-8 pt-16 flex-1">
+            <h1 class="text-3xl font-bold mb-16 text-center">
+                {{ title }}
+            </h1>
+            <nuxt/>
+        </div>
     </div>
 </template>
 
@@ -8,11 +25,66 @@
 export default {
     head: {
         link: [
-            // { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css' },
+            // { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Crimson+Text:400,400i,700,700i' },
+            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Neuton:400,400i,700' },
+            { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css' },
         ],
-        bodyAttrs: {
-            class: 'bg-black text-grey',
+    },
+
+    computed: {
+        title() {
+            return this.$route.matched[0]
+                ? this.$route.matched[0].components.default.options.title
+                : '';
         },
     },
 }
 </script>
+
+<style global>
+    * {
+        position: relative;
+        box-sizing: inherit;
+        margin: 0;
+        padding: 0;
+        color: inherit;
+        font: inherit;
+    }
+
+    *:after,
+    *:before {
+        box-sizing: inherit;
+    }
+
+    html {
+        box-sizing: border-box;
+    }
+
+    body {
+        background-color: #f5f5f5;
+        font-family: 'Neuton';
+    }
+
+    h1 {
+        font-size: inherit; /* Reset normalize 2em */
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    ol,
+    ul {
+        list-style: none;
+    }
+
+    img,
+    svg {
+        vertical-align: middle;
+    }
+
+    .transition {
+        transition: all 0.2s ease-in-out;
+    }
+</style>
+
---
+++ example/pages/ajax-cards.vue
@@ -0,0 +1,112 @@
+<template>
+    <DataComponent
+        :data="getRicks"
+        :debounce-ms="200"
+        :initial-load-delay-ms="1000"
+        :filter="filter"
+        :page="page"
+        :per-page="20"
+    >
+        <template slot-scope="{ data, visibleCount, totalCount, pages, isSlowRequest }">
+            <div class="flex justify-between mb-12 py-4 border-t border-b border-grey">
+                <p class="text-grey-dark italic">
+                    Displaying {{ visibleCount }} of {{ totalCount }} Ricks.
+                    <span v-if="isSlowRequest">Loading...</span>
+                </p>
+                <p>
+                    <button
+                        v-for="(status, value) in statusses"
+                        :key="value"
+                        class="uppercase tracking-wide ml-4"
+                        :class="filter.status == value ? 'border-b border-black' : 'text-grey-dark'"
+                        @click="filter.status = value"
+                    >
+                        {{ status }}
+                    </button>
+                </p>
+            </div>
+
+            <div class="flex flex-wrap justify-between transition" :class="isSlowRequest ? 'opacity-50' : null">
+                <article
+                    v-for="rick in data"
+                    :key="rick.id"
+                    class="w-32 flex flex-col items-center text-center mb-12"
+                >
+                    <img
+                        :src="rick.image"
+                        :alt="`Image of ${rick.name}`"
+                        width="50"
+                        class="rounded-full mb-4"
+                    >
+                    <span class="text-grey-darkest mb-1">
+                        {{ rick.name }}
+                    </span>
+                    <span class="italic text-grey-dark">
+                        {{ statusses[rick.status] }}
+                    </span>
+                </article>
+            </div>
+
+            <ul class="mt-4 flex justify-center">
+                <li v-for="p in pages" :key="p.number">
+                    <button
+                        class="mx-4"
+                        :class="p.isActive ? 'border-b border-black' : 'text-grey-dark'"
+                        @click="page = p.number"
+                    >
+                        {{ p.number }}
+                    </button>
+                </li>
+            </ul>
+        </template>
+    </DataComponent>
+</template>
+
+<script>
+import axios from 'axios';
+import DataComponent from '../../src';
+import animatedScrollTo from 'animated-scroll-to';
+
+export default {
+    title: 'Ajax card layout',
+
+    components: {
+        DataComponent,
+    },
+
+    data: () => ({
+        page: 1,
+        filter: { status: 'all' },
+
+        statusses: {
+            'all': 'All',
+            Alive: 'Alive',
+            Dead: 'Dead',
+            unknown: 'Unknown',
+        },
+    }),
+
+    methods: {
+        getRicks({ filter, page }) {
+            const baseUrl = `https://rickandmortyapi.com/api/character?page=${page}&name=Rick`;
+            const requestUrl = filter.status !== 'all' ? `${baseUrl}&status=${filter.status}` : baseUrl;
+
+            return axios.get(requestUrl).then(response => {
+                return {
+                    data: response.data.results,
+                    totalCount: response.data.info.count,
+                };
+            });
+        },
+    },
+};
+</script>
+
+<style>
+    .grid {
+        padding: 2rem;
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+        grid-gap: 4rem;
+    }
+</style>
