486be37e019ce09da4b600b3903dfd34d8df029a
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-09-18 14:39:45 +0200

    Expose QueryComponent

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 export { default } from './components/DataComponent';
+export { default as QueryComponent } from './components/QueryComponent';
+
 export { default as DataSortToggle } from './components/DataSortToggle';
 export { default as DataFilterFacet } from './components/DataFilterFacet';
