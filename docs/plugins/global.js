a5c6453141ead92365be179cf223a3dfbc1287ab
Adriaan <adriaanmarain300@gmail.com>
2018-09-19 11:05:03 +0200

    converted export to vue plugin

--- docs/plugins/global.js
+++ docs/plugins/global.js
@@ -3,4 +3,6 @@ import DataComponent from '~/../src';
 import Intro from '~/components/Intro';
 
 Vue.component('Intro', Intro);
-Vue.component('DataComponent', DataComponent);
+/* Vue.component('DataComponent', DataComponent); */
+
+Vue.use(DataComponent);
