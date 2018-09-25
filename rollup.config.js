9f01505b77a0523349218867fcc4d91a7eab6df2
Adriaan <adriaanmarain300@gmail.com>
2018-09-25 11:08:36 +0200

    babel vue jsx plugin (wip)

--- rollup.config.js
+++ rollup.config.js
@@ -13,7 +13,6 @@ export default [
             commonjs(),
             babel({
                 exclude: 'node_modules/**',
-                plugins: ['external-helpers'],
             }),
         ],
         output: [
@@ -46,7 +45,6 @@ export default [
             commonjs(),
             babel({
                 exclude: 'node_modules/**',
-                plugins: ['external-helpers'],
             }),
             uglify(),
         ],
